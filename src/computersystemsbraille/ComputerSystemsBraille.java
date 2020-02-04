296ba061f25477fc3c7c1e2cbe658cdfb4d276a3
grimaesthetic <grimaesthetic@users.noreply.github.com>
2020-02-04 12:06:50 -0500

    brother

---
+++ src/computersystemsbraille/ComputerSystemsBraille.java
@@ -0,0 +1,18 @@
+package computersystemsbraille;
+public class ComputerSystemsBraille {
+
+    public static void main(String[] args) {
+       //Unicode Braille
+       //https://www.unicode.org/charts/PDF/U2800.pdf
+       //English to Braille translator https://www.brailletranslator.org/
+       
+       String L= "\u2838";
+       String e= "\u2811";
+       String x= "\u28D2";
+       String i= "\u28DA";
+       
+        System.out.println(L + e + x + i);
+               
+    }
+    
+}
